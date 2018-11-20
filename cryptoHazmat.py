f3d8c0aa7dbac6441fd87e731b75eecebd7d8436
sajadorouji <sajad.orj@gmail.com>
2018-11-20 18:11:25 +0330

    Update

--- cryptoHazmat.py
+++ cryptoHazmat.py
@@ -7,13 +7,15 @@ import os
 class CryptoHazmat:
     def __init__(self, path):
         self.path = path
-        self.private_key = rsa.generate_private_key(public_exponent = 65537, key_size = 2048, backend = default_backend())
-        self.publickey = self.private_key.public_key()
+        self.private_key = ""
+        self.publickey = ""
 
     def createKeysfile(self):
         if not os.path.exists(self.path):
             os.makedirs(self.path)
         if not os.path.isfile(self.path + "/privatekey.key"):
+            self.private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
+            self.publickey = self.private_key.public_key()
             privateFile = open(self.path + "/privatekey.key", "w")
             privateFile.write(str(self.private_key.private_bytes(encoding = serialization.Encoding.PEM, format = serialization.PrivateFormat.TraditionalOpenSSL, encryption_algorithm = serialization.NoEncryption())))
             privateFile.close()
@@ -21,17 +23,20 @@ class CryptoHazmat:
             publicFile.write(str(self.publickey.public_bytes(encoding = serialization.Encoding.PEM, format = serialization.PublicFormat.SubjectPublicKeyInfo)))
             publicFile.close()
 
+    def encryptData(self, message):
+        pass
+    def decryptData(self, enc_message):
+        pass
 
 
 
 
-
-    def printkey(self):
-        # pem = self.private_key.private_bytes(encoding = serialization.Encoding.PEM, format = serialization.PrivateFormat.TraditionalOpenSSL, encryption_algorithm = serialization.NoEncryption())
-
-        pem = self.publickey.public_bytes(encoding = serialization.Encoding.PEM, format = serialization.PublicFormat.SubjectPublicKeyInfo)
-        return pem
-
+    # def printkey(self):
+    #     # pem = self.private_key.private_bytes(encoding = serialization.Encoding.PEM, format = serialization.PrivateFormat.TraditionalOpenSSL, encryption_algorithm = serialization.NoEncryption())
+    #
+    #     pem = self.publickey.public_bytes(encoding = serialization.Encoding.PEM, format = serialization.PublicFormat.SubjectPublicKeyInfo)
+    #     return pem
+    #
 
     # def
 
--- hazmettest.py
+++ hazmettest.py
@@ -3,4 +3,4 @@ import cryptoHazmat
 if __name__ == '__main__':
     keys = cryptoHazmat.CryptoHazmat("./sajad")
     keys.createKeysfile()
-    print(keys.printkey())
+    #print(keys.printkey())
